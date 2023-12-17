1a256c1d8a6743f277c430533e0dc1ac032248b4
gladun.vladimir <vladimir.gldn@gmail.com>
2023-12-17 21:28:31 +0300

    add chechking bytes losses after data restore

--- tests/test.py
+++ tests/test.py
@@ -28,21 +28,13 @@ def count_number_and_size(root_path, file_dir):
 
     dir_size = sum(d.stat().st_size for d in os.scandir(dir) if d.is_file())
 
-    print('File Count in ', 'Directory:  ' + dir, file_count - 1, ', Directory Size is: ', dir_size)
+    print('File count in ', 'directory:  ' + dir, file_count - 1, ', directory size is: ', dir_size)
 
 
 def get_file_marker(root_path):
     dir = root_path + 'occurrences'
     list = os.listdir(dir)
     print(list)
-
-
-def get_file_size(path_to_restored_file, path_to_saved_file):
-    saved_file_stats = os.stat(path_to_saved_file)
-    restored_file_stats = os.stat(path_to_restored_file)
-    print(f'Saved File Size in Bytes is {saved_file_stats.st_size}')
-    print(f'Restored File Size in Bytes is {restored_file_stats.st_size}')
-    return saved_file_stats.st_size - restored_file_stats.st_size
 
 
 def delete_from_dir(directory_path):
@@ -79,7 +71,7 @@ def run_dedup(alg, time_list, diff_list):
     count_number_and_size(get_root_path(), 'batches')
 
     get_file_marker(get_root_path())
-    file_marker_input = input('Input File Marker to restore: ')
+    file_marker_input = input('Input file marker to restore: ')
     count_number_and_size(get_root_path(), 'occurrences/' + file_marker_input)
 
     start_restore = datetime.datetime.now()
@@ -88,16 +80,15 @@ def run_dedup(alg, time_list, diff_list):
     work_time_restore = finish_restore - start_restore
     full_work_time = work_time_save + work_time_restore
     time_list.append(full_work_time.total_seconds())
-    print('Time for Saving File is ', work_time_save)
-    print('Time for Restoring File is ', work_time_restore)
-    difference_file_size = get_file_size(input('Input Restored File: '), PATH_TO_FILE)
-    diff_list.append(difference_file_size)
+    print('Time for saving file is ', work_time_save)
+    print('Time for restoring file is ', work_time_restore)
 
     delete_from_dir(get_root_path() + '/batches')
     delete_from_dir(get_root_path() + '/occurrences')
 
 
-def make_plot(time_list):
+
+def make_plot_work_time(time_list):
     x = ['md5', 'sha1', 'sha256', 'sha512']
     plt.bar(x, time_list, label='Seconds')
     plt.xlabel('Algoritms')
@@ -107,13 +98,57 @@ def make_plot(time_list):
     plt.show()
 
 
+def make_plot_losses():
+    x = ['md5', 'sha1', 'sha256', 'sha512']
+    plt.bar(x, diff_list, label='Bytes')
+    plt.xlabel('Algorithms')
+    plt.ylabel('Number of Losses')
+    plt.title('Losses after restore')
+    plt.legend()
+    plt.show()
+
+
+def bitwise_compare(file1, file2):
+    count = 0
+    with open(file1, "rb") as f1, open(file2, "rb") as f2:
+        while True:
+            byte1 = f1.read(1)
+            byte2 = f2.read(1)
+
+            if not byte1 and not byte2:
+                print("The files are identical.")
+                break
+            elif not byte1 or not byte2:
+                print("Files vary in size.")
+                break
+            elif byte1 != byte2:
+                print(f"Files vary per byte: {byte1} (in file {file1}) != {byte2} (in file {file2})")
+                count = count + 1
+    print('Number of different bytes', count)
+    return count
+
+
 if __name__ == "__main__":
     PATH_TO_FILE = input('Input File Path to save: ')
     diff_list = []
     time_list = []
     run_dedup('md5', time_list, diff_list)
+
+    restored_file_path = input('Input restored file: ')
+    diff_list.append(bitwise_compare(PATH_TO_FILE, restored_file_path))
+
     run_dedup('sha1', time_list, diff_list)
+
+    diff_list.append(bitwise_compare(PATH_TO_FILE, restored_file_path))
+
     run_dedup('sha256', time_list, diff_list)
+
+    diff_list.append(bitwise_compare(PATH_TO_FILE, restored_file_path))
+
     run_dedup('sha512', time_list, diff_list)
-    make_plot(time_list)
+
+    diff_list.append(bitwise_compare(PATH_TO_FILE, restored_file_path))
+
+    make_plot_work_time(time_list)
+    make_plot_losses()
     print(diff_list)
